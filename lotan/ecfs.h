538678b0abee7f9fd23ccc1b5979e550251e8a8e
Ryan O'Neill <elfmaster@zoho.com>
2015-01-22 13:28:23 -0800

    ...

--- lotan/ecfs.h
+++ lotan/ecfs.h
@@ -17,13 +17,13 @@
 #include <locale.h>
 #include <signal.h>
 #include <sys/user.h>
-#include <sys/procfs.h> /* struct elf_prstatus */
-#include <sys/resource.h> 
+#include <sys/procfs.h>		/* struct elf_prstatus */
+#include <sys/resource.h>
 #include "dwarf.h"
 #include "libdwarf.h"
 
 #define PAGE_ALIGN(x) (x & ~(PAGE_SIZE - 1))
-#define PAGE_ALIGN_UP(x) (PAGE_ALIGN(x) + PAGE_SIZE) 
+#define PAGE_ALIGN_UP(x) (PAGE_ALIGN(x) + PAGE_SIZE)
 
 #define MAX_TID 256
 #define PT_ATTACHED 1
@@ -42,7 +42,7 @@
 #define PS_RUNNING 32
 #define PS_UNKNOWN 64
 
-#define MAX_THREADS 256 // for each threads prstatus
+#define MAX_THREADS 256		// for each threads prstatus
 
 #define ELFNOTE_NAME(_n_) ((char*)(_n_) + sizeof(*(_n_)))
 #define ELFNOTE_ALIGN(_n_) (((_n_)+3)&~3)
@@ -51,31 +51,42 @@
 #define ELFNOTE_NEXT(_n_) ((ElfW(Nhdr) *)(ELFNOTE_DESC(_n_) + ELFNOTE_ALIGN((_n_)->n_descsz)))
 
 struct opts {
-        int coretype;
-        int all;
-        int pid;
-        char *snapdir;
+	int coretype;
+	int all;
+	int pid;
+	char *snapdir;
 };
 
 typedef struct {
-        Elf64_Word namesz;
-        Elf64_Word descsz;
-        Elf64_Word type;
+	Elf64_Word namesz;
+	Elf64_Word descsz;
+	Elf64_Word type;
 } ElfW(Note);
 
-struct fde_func_data { /* For eh_frame.c */ 
-        uint64_t addr;
-        size_t size;
+struct fde_func_data {		/* For eh_frame.c */
+	uint64_t addr;
+	size_t size;
 };
 
+struct section_meta_data {
+	ElfW(Addr) bssVaddr, dynVaddr, relVaddr, relaVaddr, ehframeVaddr,
+	    textVaddr, o_textVaddr, dataVaddr, o_dataVaddr, gotVaddr, noteVaddr,
+	    hashVaddr, initVaddr, finiVaddr, pltVaddr, dsymVaddr, dstrVaddr,
+	    interpVaddr, tlsVaddr;
+	ElfW(Off) bssOff, dynOff, relOff, relaOff, noteOff, ehframeOff,
+	    textOffset, dataOffset, gotOff, hashOff, initOff, finiOff, pltOff,
+	    dsymOff, dstrOff, interpOff, tlsOff;
+	ElfW(Word) bssSiz, dynSiz, hashSiz, ehframeSiz, textfSize, textSize,
+	    dataSize, strSiz, pltSiz, interpSiz, tlsSiz, noteSiz, dsymSiz,
+	    dstrSiz;
+};
 
 struct elf_thread_core_info {
-        struct elf_prstatus *prstatus;
+	struct elf_prstatus *prstatus;
 	struct elf_prpsinfo *psinfo;
 	siginfo_t *siginfo;
-	ElfW(Nhdr) *notes;
+	 ElfW(Nhdr) * notes;
 };
-
 
 struct nt_file_struct {
 	struct {
@@ -89,44 +100,44 @@ struct nt_file_struct {
 };
 
 typedef struct notedesc {
-        ElfW(Nhdr) *notes;
-        struct elf_prstatus *prstatus;  /* NT_PRSTATUS */
-        struct elf_prpsinfo *psinfo;    /* NT_PRPSINFO */
+	ElfW(Nhdr) * notes;
+	struct elf_prstatus *prstatus;	/* NT_PRSTATUS */
+	struct elf_prpsinfo *psinfo;	/* NT_PRPSINFO */
 #if __x86_64__
 	Elf64_auxv_t *auxv;
 #else
 	Elf32_auxv_t *auxv;
 #endif
 	struct siginfo_t *siginfo;
-        struct elf_thread_core_info thread_core_info[MAX_THREADS];
+	struct elf_thread_core_info thread_core_info[MAX_THREADS];
 	elf_fpregset_t *fpu;
 	int thread_count;
-        int thread_status_size;
-        int numnote;
+	int thread_status_size;
+	int numnote;
 	struct nt_file_struct *nt_files;
 } notedesc_t;
 
 struct coredump_params {
-        siginfo_t *siginfo;
+	siginfo_t *siginfo;
 	struct pt_regs *regs;
-        unsigned long limit;
-        unsigned long mm_flags;
+	unsigned long limit;
+	unsigned long mm_flags;
 };
 
 typedef struct elfdesc {
 	uint8_t *mem;
-	ElfW(Ehdr) *ehdr;
-	ElfW(Phdr) *phdr;
-	ElfW(Shdr) *shdr;
-	ElfW(Nhdr) *nhdr;
-	ElfW(Addr) textVaddr;
-	ElfW(Addr) dataVaddr;
-	ElfW(Addr) dynVaddr;
-	ElfW(Addr) ehframe_Vaddr;
-	ElfW(Addr) noteVaddr;
-	ElfW(Off) textOffset;
-	ElfW(Off) dataOffset;
-	ElfW(Off) dynamicOffset;
+	 ElfW(Ehdr) * ehdr;
+	 ElfW(Phdr) * phdr;
+	 ElfW(Shdr) * shdr;
+	 ElfW(Nhdr) * nhdr;
+	 ElfW(Addr) textVaddr;
+	 ElfW(Addr) dataVaddr;
+	 ElfW(Addr) dynVaddr;
+	 ElfW(Addr) ehframe_Vaddr;
+	 ElfW(Addr) noteVaddr;
+	 ElfW(Off) textOffset;
+	 ElfW(Off) dataOffset;
+	 ElfW(Off) dynamicOffset;
 	char *StringTable;
 	size_t size;
 	size_t noteSize;
@@ -145,7 +156,7 @@ typedef struct mappings {
 	unsigned long base;
 	size_t size;
 	int elfmap;
-	int textbase; // is the text segment base of the processes exe
+	int textbase;		// is the text segment base of the processes exe
 	int stack;
 	int thread_stack;
 	int heap;
@@ -164,13 +175,13 @@ typedef struct mappings {
 } mappings_t;
 
 typedef struct memdesc {
-	pid_t pid;	
-	uint8_t *exe; /* Points to /proc/<pid>/exe */
-	char *path;   // path to executable
-	char *comm; //name of executable
-	int mapcount; // overall # of memory maps
-	int type; // ET_EXEC or ET_DYN
-	ElfW(Addr) base, data_base;
+	pid_t pid;
+	uint8_t *exe;		/* Points to /proc/<pid>/exe */
+	char *path;		// path to executable
+	char *comm;		//name of executable
+	int mapcount;		// overall # of memory maps
+	int type;		// ET_EXEC or ET_DYN
+	 ElfW(Addr) base, data_base;
 	struct {
 		unsigned long sh_offset;
 		unsigned long base;
@@ -191,7 +202,7 @@ typedef struct memdesc {
 		unsigned long base;
 		unsigned int size;
 	} heap;
-	struct { 
+	struct {
 		int fds[MAXFD];
 		int pid;
 		int ppid;
@@ -201,7 +212,7 @@ typedef struct memdesc {
 		int exit_signal;
 		pid_t tid[MAX_TID];
 		pid_t leader;
-		pid_t tracer; // the pid of the tracer
+		pid_t tracer;	// the pid of the tracer
 		unsigned int state;
 	} task;
 	mappings_t *maps;
@@ -211,9 +222,7 @@ typedef struct memdesc {
 	uint8_t *saved_auxv;
 	int pie;
 } memdesc_t;
-	
-		
-	
+
 typedef struct descriptor {
 	elfdesc_t binary;
 	memdesc_t memory;
@@ -223,23 +232,19 @@ typedef struct descriptor {
 	char *snapdir;
 } desc_t;
 
-
 typedef struct node {
-        struct node *next;
-        struct node *prev;
-        desc_t *desc;
+	struct node *next;
+	struct node *prev;
+	desc_t *desc;
 } node_t;
 
 typedef struct list {
-        node_t *head;
-        node_t *tail;
+	node_t *head;
+	node_t *tail;
 } list_t;
 
-
-
-memdesc_t * take_process_snapshot(pid_t);
-void * heapAlloc(size_t);
-char * xstrdup(const char *);
-char * xfmtstrdup(char *fmt, ...);
+memdesc_t *take_process_snapshot(pid_t);
+void *heapAlloc(size_t);
+char *xstrdup(const char *);
+char *xfmtstrdup(char *fmt, ...);
 int get_all_functions(const char *filepath, struct fde_func_data **funcs);
-
