f8ad0e423e41eb89e2fba18f7a46284ea4579361
Ryan O'Neill <elfmaster@zoho.com>
2015-05-28 15:05:54 -0700

    added detect_preloaded_libs

--- src/core_accessors.c
+++ src/core_accessors.c
@@ -113,6 +113,62 @@ static int is_elf_mapping(uint8_t *mem)
 	return ehdr->e_type;
 }
 
+#define STACK_CHUNK_SIZE 8192
+#define MAX_PRELOADS 64
+
+ssize_t detect_preloaded_libs(handle_t *handle, struct lib_mappings *lm, struct elfmap **elfmaps)
+{
+	elfdesc_t *elfdesc = handle->elfdesc;
+        memdesc_t *memdesc = handle->memdesc;
+        ElfW(Phdr) *phdr = elfdesc->phdr;
+        uint8_t *mem = elfdesc->mem;
+	uint8_t *stack_ptr;
+	uint64_t stack_offset;
+	int i;
+
+	stack_offset = get_internal_sh_offset(elfdesc, memdesc, STACK);
+	for (i = 0; i < elfdesc->ehdr->e_phnum; i++) {
+		if (phdr[i].p_offset == stack_offset) {
+			/*
+		         * XXX change to 'char **preloaded' and use realloc's on sizeof(char *) * N
+			 */
+			char *preloaded[MAX_PRELOADS], *p;
+			int prld_index, c;
+			const int ldplen = strlen("LD_PRELOAD=");
+			/*
+			 * The environment variable strings are going to be right
+			 * after the auxiliary vector. Somewhere in the first 8192
+			 * bytes from the bottom of the stack.
+		         */
+			stack_ptr = (uint8_t *)&mem[phdr[i].p_offset + phdr[i].p_memsz - (STACK_CHUNK_SIZE)];			
+			for (i = 0; i < STACK_CHUNK_SIZE; i++) {
+				if (!strncmp(&stack_ptr[i], "LD_PRELOAD=", ldplen)) {
+					p = &stack_ptr[i + ldplen];
+					if (*p == '"')
+						p++;
+					preloaded[prld_index] = (char *)heapAlloc(MAX_PATH);
+					for (prld_index = 0, c = 0; *p != '\0' && *p != '"';) {
+						preloaded[prld_index][c++] = *p; 
+						/*
+						 * if there's a space instead of a null byte
+						 * then multiple shared libs are given.
+						 */
+						if((*p + 1) == 0x20) {
+							preloaded[prld_index][c] = '\0';
+							preloaded[++prld_index] = (char *)heapAlloc(MAX_PATH);
+							c = 0;
+							continue;
+						} 
+					}
+					done:
+					preloaded[prld_index][c] = '\0';
+				}
+			}
+		}	
+	}
+
+}
+
 ssize_t check_segments_for_elf_objects(handle_t *handle, struct lib_mappings *lm, struct elfmap **elfmaps)
 {
 	elfdesc_t *elfdesc = handle->elfdesc;
